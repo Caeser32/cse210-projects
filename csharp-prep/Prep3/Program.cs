78e99e9c011c7ecca559f29f342ae44c0afa7136
Caeser32 <btdmsiza@gmail.com>
2024-04-27 20:34:46 +0200

    completed C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,53 @@
 using System;
 
-class Program
+namespace Prep3
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep3 World!");
+        static void Main(string[] args)
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            int guess;
+            int attempts = 0;
+            bool guessedCorrectly = false;
+
+            Console.WriteLine("Welcome to Guess My Number!");
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                attempts++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    guessedCorrectly = true;
+                }
+            } while (!guessedCorrectly);
+
+            Console.WriteLine($"It took you {attempts} attempts to guess the magic number.");
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            string playAgain = Console.ReadLine();
+            
+            if (playAgain.ToLower() == "yes")
+            {
+                Main(args);
+            }
+            else
+            {
+                Console.WriteLine("Thanks for playing!");
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,65 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace Prep4
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep4 World!");
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            int input;
+
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+            do
+            {
+                Console.Write("Enter number: ");
+                input = int.Parse(Console.ReadLine());
+
+                if (input != 0)
+                {
+                    numbers.Add(input);
+                }
+            } while (input != 0);
+
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+
+            double average = (double)sum / numbers.Count;
+
+            int max = numbers[0];
+            foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {max}");
+            
+            int smallestPositive = int.MaxValue;
+            foreach (int number in numbers)
+            {
+                if (number > 0 && number < smallestPositive)
+                {
+                    smallestPositive = number;
+                }
+            }
+
+            numbers.Sort();
+            Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+            Console.WriteLine("The sorted list is:");
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
     }
 }
