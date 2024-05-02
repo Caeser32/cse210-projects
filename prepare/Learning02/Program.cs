7714d79a8983c28c261dd1f2cb493a0c0f0b31f7
Caeser32 <btdmsiza@gmail.com>
2024-05-02 23:02:03 +0200

    completed the Abstraction Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,32 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._company = "Oishi";
+        job1._jobTitle = "Delivery driver";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._company = "BYU-Pathway";
+        job2._jobTitle = "Peer-advisor";
+        job2._startYear = 2023;
+        job2._endYear = 2026;
+
+        job1.DisplayJobDetails();
+        job2.DisplayJobDetails();
+
+
+        Resume myResume = new Resume();
+        myResume._personName = "Tumisang Msiza";
+
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
