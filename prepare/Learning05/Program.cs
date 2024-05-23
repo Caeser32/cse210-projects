20306dab3ad3fe6dc95d81cf5aa61512f5bcad6c
Caeser32 <btdmsiza@gmail.com>
2024-05-23 14:58:01 +0200

    completed learning actiivty 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 5, 3));
+        shapes.Add(new Circle("Green", 2.5f));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
